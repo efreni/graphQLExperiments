7de9f1d59b52523b3e3edcb7ed2cbbc622e0dd1a
Enrico <enrico@freni.it>
2020-03-03 21:05:23 +0100

    pre-api status

---
+++ src/math.js
@@ -0,0 +1,9 @@
+sumUpTwoNumbers = (numberOne, NumberTwo) => {
+  return numberOne + NumberTwo;
+};
+
+detractUpTwoNumbers = (numberOne, NumberTwo) => {
+  return numberOne - NumberTwo;
+};
+
+export { sumUpTwoNumbers as default, detractUpTwoNumbers };
