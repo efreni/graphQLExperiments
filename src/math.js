973039b786702d1612f4f8a317f99ba55bd37c64
Enrico <enrico@freni.it>
2020-03-04 06:42:27 +0100

    Clean up

--- src/math.js
+++
@@ -1,9 +0,0 @@
-sumUpTwoNumbers = (numberOne, NumberTwo) => {
-  return numberOne + NumberTwo;
-};
-
-detractUpTwoNumbers = (numberOne, NumberTwo) => {
-  return numberOne - NumberTwo;
-};
-
-export { sumUpTwoNumbers as default, detractUpTwoNumbers };
