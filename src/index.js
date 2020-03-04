b355da12eec7bf2fab306da009532a937baf2fcc
Enrico <enrico@freni.it>
2020-03-04 07:26:43 +0100

    Test queries with null values

--- src/index.js
+++ src/index.js
@@ -1,29 +1,56 @@
 import { GraphQLServer } from "graphql-yoga";
 
+// 5 Scalar types = String, Boolean, Int, Float, ID
+// scalar type means that cointains a single value type (differently from an object for ex)
+
 //type definitions(schema)
 const typeDefs = `
     type Query {
-        hello: String!
+        id: ID!
         name: String!
-        location: String!
-        bio: String!
-    }
-    `;
+        age: Int!
+        employed: Boolean!
+        gpa: Float
+        title: String!
+        price: Int!
+        releaseYear: Int
+        rating: Int
+        inStock: Boolean!
+    } 
+    `; //! <- means it has to return always a value of that type, otherwise can be null
 
 //resolvers
 const resolvers = {
   Query: {
-    hello() {
-      return "This is my first query";
+    id() {
+      return "abc123";
     },
     name() {
-      return "Enrico F";
+      return "Enrico";
     },
-    location() {
-      return "Florence";
+    age() {
+      return 33;
     },
-    bio() {
-      return "Junior Software Developer";
+    employed() {
+      return true;
+    },
+    gpa() {
+      return null;
+    },
+    title() {
+      return "An Item Name";
+    },
+    price() {
+      return 100;
+    },
+    releaseYear() {
+      return null;
+    },
+    rating() {
+      return null;
+    },
+    inStock() {
+      return true;
     }
   }
 };
