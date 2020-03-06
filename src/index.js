7bd4c39450549f953cbfd209a7541aafffc8b98f
Enrico Freni <enrico.freni@temera.it>
2020-03-06 17:36:54 +0100

    add comment and post creation with user, email and post validation on creation

--- src/index.js
+++ src/index.js
@@ -72,6 +72,8 @@ const typeDefs = `
 
   type Mutation {
     createUser(name: String!, email: String!, age: Int): User!
+    createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+    createComment(text: String!, author: ID!, post: ID! ): Comment!
   } 
 
   type User {
@@ -166,11 +168,11 @@ const resolvers = {
     }
   },
   Mutation: {
-    createUser(parent, args, ctx, info){
+    createUser(parent, args, ctx, info) {
       const emailTaken = users.some((user) => {
         return user.email === args.email
       })
-      if (emailTaken){
+      if (emailTaken) {
         throw new Error('Email already taken')
       }
 
@@ -184,6 +186,56 @@ const resolvers = {
       users.push(user)
 
       return user
+    },
+    createPost(parent, args, ctx, info) {
+      const userExists = users.some((user) => {
+        return user.id === args.author
+      })
+      
+      if(!userExists){
+        throw new Error("User not found")
+      }
+
+      const post = {
+        id: uuidv4(),
+        title: args.title,
+        body:args.body,
+        published: args.published,
+        author: args.author
+      }
+
+      posts.push(post) //why am I pushing this?
+      
+      return post
+      
+    },
+    createComment(parent, args, ctx, info){
+      const userExists = users.some((user) => {
+        return user.id === args.author
+      })
+
+      const postExists = posts.some((post) => {
+        return post.id === args.post && post.published
+      })
+      
+      if(!userExists){
+        throw Error("The specified user doesn't exist")
+      }
+      if(!postExists){
+        throw Error("The specified post doesn't exist")
+      }
+
+      const comment = {
+        id: uuidv4(),
+        text: args.text,
+        author: args.author,
+        post: args.post
+      }
+
+      comments.push(comment) //why am I pushing this?
+
+      return comment
+
     }
   }
 }
