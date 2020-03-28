2b7dc1c0288fed11c85c964a2a64eb046b0a7305
Enrico <enrico@freni.it>
2020-03-28 14:47:18 +0100

    Added update mutations for users, posts and comments

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -40,6 +40,34 @@ const Mutation = {
 
     return deletedUsers[0];
   },
+  updateUser(parent, args, { db }, info) {
+    const { id, data } = args;
+    const user = db.users.find(user => user.id === id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    if (typeof data.email === "string") {
+      const emailTaken = db.user.some(user => user.email === data.email);
+
+      if (emailTaken) {
+        throw new Error("Email has already been used");
+      }
+
+      user.email = data.email;
+    }
+
+    if (typeof data.name === "string") {
+      user.name = data.name;
+    }
+
+    if (typeof data.age !== "undefined") {
+      user.age = data.age;
+    }
+
+    return user;
+  },
   createPost(parent, args, { db }, info) {
     const userExists = db.users.some(user => user.id === args.data.author);
 
@@ -53,6 +81,28 @@ const Mutation = {
     };
 
     db.posts.push(post); //why am I pushing this?
+
+    return post;
+  },
+  updatePost(parent, args, { db }, info) {
+    const { id, data } = args;
+    const post = db.posts.find(post => post.id === id);
+
+    if (!post) {
+      throw new Error("Post not found");
+    }
+
+    if (typeof data.title === "string") {
+      post.title = data.title;
+    }
+
+    if (typeof data.body === "string") {
+      post.body = data.body;
+    }
+
+    if (typeof data.published === "boolean") {
+      post.published = data.published;
+    }
 
     return post;
   },
@@ -89,6 +139,20 @@ const Mutation = {
     };
 
     db.comments.push(comment); //why am I pushing this?
+
+    return comment;
+  },
+  updateComment(parent, args, { db }, info) {
+    const { id, data } = args;
+    const comment = db.comments.find(comment => comment.id === id);
+
+    if (!comment) {
+      throw new Error("Comment not found");
+    }
+
+    if (typeof data.text === "string") {
+      comment.text = data.text;
+    }
 
     return comment;
   },
